89ab160916c1097361d5f97e626807c68c8423bc
Alexandr <ovdienkoalexandr@mail.ru>
2017-06-09 11:26:07 +0300

    Кнопка входа и about

--- Сайт/SpaSalon/SpaSalon/urls.py
+++ Сайт/SpaSalon/SpaSalon/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from store import views
 
 urlpatterns = [
-                  url(r'^admin/', admin.site.urls),
+                  url(r'^admin/', admin.site.urls, name='admin'),
                   url(r'^admin/register/$', views.RegisterFormView.as_view(), name='register'),
                   url(r'^SpaSalon/', include('store.urls'), name='SpaSalon'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
